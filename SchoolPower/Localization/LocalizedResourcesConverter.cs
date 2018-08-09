f067b618cbfb6a9e9d20243c4b4d9b7d21f6addf
AOIDUO <30717258+UniverseObserver@users.noreply.github.com>
2018-08-09 16:22:08 +0800

    Localization && Refactor

---
+++ SchoolPower/Localization/LocalizedResourcesConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace SchoolPower.Localization {
+    public class LocalizedResourcesConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language) {
+            if (value == null) return string.Empty;
+
+            var languageCode = (string)value;
+
+            return new CultureInfo(languageCode).DisplayName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) {
+            return value;
+        }
+    }
+}
